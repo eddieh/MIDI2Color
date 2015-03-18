e849f4253dd3989440868aeacab26c146293673e
Eddie Hillenbrand <eddie.hillenbrand@adcolony.com>
2015-03-18 07:57:50 -0700

    Initial Commit

---
+++ MIDI2Color/MIDI2ColorPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  MIDI2ColorPlugIn.h
+//  MIDI2Color
+//
+//  Created by Eddie Hillenbrand on 3/18/15.
+//  Copyright (c) 2015 Eddie Hillenbrand. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface MIDI2ColorPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
